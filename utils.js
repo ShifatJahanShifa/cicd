c6380b8257dcab9f00c97c166f53a2d8038c4b7c
ShifatJahanShifa <bsse1301@iit.du.ac.bd>
2025-01-07 09:36:01 +0600

    right case

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 function add(a, b) {
     if (isNaN(a)) return "Invalid Input";
     if (isNaN(b)) return "Invalid Input";
-    return parseInt(a) * parseInt(b);
+    return parseInt(a) + parseInt(b);
 }
 module.exports = add;
